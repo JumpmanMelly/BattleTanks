0f8c9ebb1af1f932070a3992780db5c360d50377
Melvin Yates <woodsong90@gmail.com>
2022-03-30 16:38:48 -0400

    Readjust testing for tank treads; Path adjustments to no longer duplicate manually

--- objects/Tank Parts/Test.cs
+++ objects/Tank Parts/Test.cs
@@ -3,28 +3,51 @@ using Godot.Collections;
 
 namespace BattleTanks.TankParts
 {
-public class Test : Spatial
-{
-    [Export]
-    public float Speed = 50f;
-    private float _time = 0f;
-    private float _offset = 0.86f;
-
-    private Array links;
-
-    public override void _Ready()
+    public class Test : Spatial
     {
-        links = GetNode<Path>("Path").GetChildren();
-    }
-    public override void _Process(float delta)
-    {
-        _time += delta;
-        int count =0;
-        foreach (PathFollow index in links)
+        [Export]
+        public float Speed = 20f;
+
+        [Export]
+        public float Offset = 0.7f;
+
+        [Export]
+        public int Tracks = 25;
+
+        private Array _links;
+        private float _time = 0f;
+
+        public override void _Ready()
         {
-            index.Offset = Speed * _time + (_offset * count);
-            count += 1;
+            for (int i = 1; i <= Tracks; i += 1)
+            {
+                var newLink = MakeLink();
+                GetNode<Path>("Path").AddChild(newLink);
+            }
+
+            _links = GetNode<Path>("Path").GetChildren();
+        }
+        public override void _Process(float delta)
+        {
+            _time += delta;
+            int count = 0;
+            foreach (PathFollow index in _links)
+            {
+                index.Offset = Speed * _time + (Offset * count);
+                count += 1;
+            }
+        }
+
+        private PathFollow MakeLink()
+        {
+            PathFollow linkPath = new PathFollow();
+            linkPath.RotationMode = PathFollow.RotationModeEnum.Oriented;
+
+            PackedScene linkPs = GD.Load<PackedScene>("res://objects/Tank Parts/Link.tscn");
+            var link = linkPs.Instance();
+            linkPath.AddChild(link);
+            
+            return linkPath;
         }
     }
-}
 }
